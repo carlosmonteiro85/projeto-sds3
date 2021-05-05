0967b7a4c6c34a08aabd1bdf9b6ea28e4c3269b7
carlosmonteiro85 <carlosmonteiro1985@outlook.com>
2021-05-05 09:40:46 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import Navbar from "components/navbar";
 
 
 function App() {
   return (
-   <div>
-      <h1 className="text-primary">Ola Mundo</h1>
-   </div>
+    /* QUANDO HA DUAS FUNÇÕES COLOCAR O FRAGMENT < ></>*/
+    <>
+      <Navbar/>
+      <div className="container">
+          <h1 className="text-primary">Ola Mundo</h1>
+          <DataTable />
+      </div>
+
+      <Footer/>
+   </>
   );
 }
 
