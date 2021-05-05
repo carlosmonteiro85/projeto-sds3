19c9e2d774632c5cae786f2c201ce8cbfb28a3fd
carlosmonteiro85 <carlosmonteiro1985@outlook.com>
2021-05-05 10:50:14 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
+import DunotChats from "components/DunotCharts";
 import Footer from "components/Footer";
 import Navbar from "components/navbar";
 
@@ -9,7 +11,24 @@ function App() {
     <>
       <Navbar/>
       <div className="container">
-          <h1 className="text-primary">Ola Mundo</h1>
+          <h1 className="text-primary py-3">DashBord Vendas</h1>
+            {/* lINHA COM 12 COLUNAS, COM ESPAÇAMENTO DE 3 PX */}
+          <div className="row px-3">
+            {/* COLUNA COM METADE DA LINHA COM ESPAÇAMENTO DE 3 PX */}
+              <div className="col-sm-6">
+                  <h5 className="text-center text-secondary">taxa de suceso (%)</h5>
+                   <BarChart /> 
+              </div>
+               {/* COLUNA QUE FECHA O TOTAL DA ROW*/}
+              <div className="col-sm-6">
+                  <h5 className="text-center text-secondary">Todas Vendas</h5>
+                  <DunotChats/> 
+              </div>
+          </div>
+
+          <div className="py-3 ">
+              <h2 className="text-primary">Todas as Vendas</h2>
+          </div>
           <DataTable />
       </div>
 
